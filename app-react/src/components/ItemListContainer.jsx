2fa49df6475ef1b5dee47d9e9b9051a1f2ad03bb
LucaConsiglio27 <lsconsiglio@hotmail.com>
2024-03-13 12:24:58 -0300

    arreglo itemListContainer.css

--- app-react/src/components/ItemListContainer.jsx
+++ app-react/src/components/ItemListContainer.jsx
@@ -1,12 +1,12 @@
 // ItemListContainer.jsx
 import React from 'react';
-import './ItemListContainer.css'; // Importa el archivo de estilos CSS
+import './ItemListContainer.css';
 
 export default function ItemListContainer({ greeting }) {
     return (
         <>
             <section>
-                <h2 className="greeting">{greeting}</h2> {/* Aplica la clase de estilo al mensaje */}
+                <h2 className="greeting">{greeting}</h2>
             </section>
         </>
     );
