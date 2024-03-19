23e7040af2ed327100564c72c551d7fcaaef42e1
LucaConsiglio27 <lsconsiglio@hotmail.com>
2024-03-19 13:01:49 -0300

    nuevo

--- app-react/src/components/ItemListContainer.jsx
+++ app-react/src/components/ItemListContainer.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import './ItemListContainer.css';
 
 export default function ItemListContainer({ greeting }) {
+
+
     return (
         <>
             <section>
                 <h2 className="greeting">{greeting}</h2>
             </section>
+
+        
         </>
     );
 }
