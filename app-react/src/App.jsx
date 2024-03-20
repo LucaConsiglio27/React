d3bc3ecd368a12a0821797eacfce4cdd7bff454a
LucaConsiglio27 <lsconsiglio@hotmail.com>
2024-03-20 13:58:13 -0300

    arreglos navegacion

--- app-react/src/App.jsx
+++ app-react/src/App.jsx
@@ -1,12 +1,11 @@
-// Importa los componentes necesarios
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Lacteos from '../src/components/Lacteos';
 import Postres from '../src/components/Postres';
 import Nosotros from '../src/components/Nosotros';
 import NavBar from '../src/components/NavBar';
-import ItemListContainer from '../src/components/Containers/ItemListContainer';
-import ItemDetailContainer from './components/Containers/ItemDetailContainer';
+import ItemListContainer from '../src/components/Containers/ItemListContainer'; // Agregué la importación faltante
+import ItemDetailContainer from '../src/components/Containers/ItemDetailContainer';
 
 const App = () => {
     return (
@@ -14,16 +13,16 @@ const App = () => {
             <div>
                 <NavBar />
                 <Routes>
-                    <Route path="/" element={<ItemListContainer />} /> 
+                    <Route path="/" element={<h1>Bienvenido a la página de inicio</h1>} />
                     <Route path="/nosotros" element={<Nosotros />} />
                     <Route path="/lacteos" element={<Lacteos />} />
                     <Route path="/postres" element={<Postres />} />
-                    <Route path="/category/:categoryId" element={<ItemListContainer />} />
+                    <Route path="/category/:categoryId" element={<ItemListContainer />} /> {/* Corregí el nombre del componente */}
                     <Route path="/item/:itemId" element={<ItemDetailContainer />} />
                 </Routes>
             </div>
         </BrowserRouter>
     );
-}
+};
 
 export default App;
--- app-react/src/components/Containers/ItemDetailContainer.jsx
+++ app-react/src/components/Containers/ItemDetailContainer.jsx
@@ -1,31 +1,28 @@
 import '../css/ItemDetailContainer.css';
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'; // Agregué la importación faltante
 import { getProductById } from '../../asyncMock';
 import ItemDetail from '../ItemDetail';
-import { useParams } from 'react-router-dom'; 
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState(null);
-    const { itemId } = useParams(); 
+    const { itemId } = useParams(); // Corregí el nombre de la variable
 
     useEffect(() => {
-        const getProduct = async () => {
-            try {
-                const response = await getProductById(itemId); 
+        getProductById(itemId) // Usé la variable itemId en lugar de 'ItemId' como cadena
+            .then(response => {
                 setProduct(response);
-            } catch (error) {
+            })
+            .catch(error => {
                 console.error(error);
-            }
-        };
-
-        getProduct();
-    }, [itemId]); 
+            });
+    }, [itemId]);
 
     return (
         <div className='ItemDetailContainer'>
-            {product && <ItemDetail {...product} />} 
+            {product && <ItemDetail {...product} />} {/* Asegurándome de que product no sea null antes de renderizar */}
         </div>
     );
-}
+};
 
 export default ItemDetailContainer;
