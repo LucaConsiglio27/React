82d46e7d503e765203ee7b277d500e3ba284b0c7
LucaConsiglio27 <lsconsiglio@hotmail.com>
2024-03-27 10:56:59 -0300

    arreglo pre-entrega

--- app-react/src/components/Containers/ItemDetailContainer.jsx
+++ app-react/src/components/Containers/ItemDetailContainer.jsx
@@ -1,28 +1,51 @@
-import '../css/ItemDetailContainer.css';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'; // Agregué la importación faltante
-import { getProductById } from '../../asyncMock';
+import React, { useState, useEffect } from 'react';
+import { fetchProductById } from '../../asyncMock'; // Importa fetchProductById desde asyncMock
 import ItemDetail from '../ItemDetail';
 
-const ItemDetailContainer = () => {
+const ItemDetailContainer = ({ match }) => {
     const [product, setProduct] = useState(null);
-    const { itemId } = useParams(); // Corregí el nombre de la variable
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        getProductById(itemId) // Usé la variable itemId en lugar de 'ItemId' como cadena
-            .then(response => {
-                setProduct(response);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }, [itemId]);
+        const getItemDetail = async () => {
+            try {
+                const fetchedProduct = await fetchProductById(match.params.itemId);
+                setProduct(fetchedProduct);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching product detail:', error);
+                setError(error);
+                setLoading(false);
+            }
+        };
+
+        getItemDetail();
+    }, [match.params.itemId]);
+
+    if (loading) {
+        return <div>Cargando...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
 
     return (
-        <div className='ItemDetailContainer'>
-            {product && <ItemDetail {...product} />} 
+        <div>
+            {product && (
+                <ItemDetail
+                    id={product.id}
+                    name={product.name}
+                    img={product.image}
+                    description={product.description}
+                    categoria={product.category}
+                    precio={product.precio}
+                    stock={product.stock}
+                />
+            )}
         </div>
     );
-};
+}
 
 export default ItemDetailContainer;
